174fbc6006c8d3bb2adca749d7ff9d8713b0614b
devCelKyo <sangoku80090@gmail.com>
2023-02-25 20:11:40 +0100

    intents add

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import discord
 import os
 
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents)
 client.run(os.environ['hashirama_bot_token'])

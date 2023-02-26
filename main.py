7253910c7f2211070e5257918e3e1a9701d344e6
devCelKyo <sangoku80090@gmail.com>
2023-02-26 13:36:58 +0100

    asyncio fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from asyncio import run
 
 from cogs.bases import Bases
 
@@ -7,6 +8,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 Bot = discord.ext.commands.Bot(command_prefix='!', intents=intents)
-Bot.add_cog(Bases(Bot))
+async def add_cogs():
+    await Bot.add_cog(Bases(Bot))
+    # add all others cogs
 
+run(add_cogs)
 Bot.run(os.environ['hashirama_bot_token'])

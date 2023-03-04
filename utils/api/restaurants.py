2dfad4c3abfe1707e09e07d5854acbaee0333007
devCelKyo <sangoku80090@gmail.com>
2023-03-04 01:42:33 +0100

    bot api calls to backend refill

--- utils/api/restaurants.py
+++ utils/api/restaurants.py
@@ -1,12 +1,23 @@
 import requests
+import discord
+import assets.restaurants
 
 def get_restaurants(discord_id):
     response = requests.get(f"http://localhost:8000/restaurants/get_restaurants/{discord_id}").json()
     return response
 
 def buy_restaurant(discord_id):
-    response = requests.get(f"http://localhost:8000/restaurants/add_restaurant/{discord_id}").json()
-    return response
+    response = requests.get(f"http://localhost:8000/restaurants/buy_restaurant/{discord_id}")
+    if response["error"] == "True":
+        title = "Error !"
+        description = response["message"]
+        colour = discord.Colour.brand_red()
+    else:
+        title = "Restaurant Purchase"
+        description = f"You succesfully purchased a restaurant!"
+        colour = discord.Colour.brand_green()
+    
+    return title, description, colour
 
 def update_restaurant(restaurant_id):
     requests.get(f"http://localhost:8000/restaurants/update_restaurant/{restaurant_id}")
@@ -16,4 +27,30 @@ def update_user_restaurants(discord_id):
 
 def claim_shops(discord_id):
     response = requests.get(f"http://localhost:8000/restaurants/claim_restaurants/{discord_id}").json()
-    return response
+    if response["error"] == "True":
+        title = "Error !"
+        description = response["message"]
+        colour = discord.Colour.brand_red()
+        img_url = assets.restaurants.FAIL
+    else:
+        title = "Revenue Redeemed!"
+        description = f"You redeemed what your restaurants earned and got {response['given_money']}両!"
+        colour = discord.Colour.brand_green()
+        img_url = assets.restaurants.RYO
+    
+    return title, description, colour, img_url
+
+def refill_all(discord_id):
+    response = requests.get(f"http://localhost:8000/restaurants/refill_restaurants/{discord_id}").json()
+    if response["error"] == "True":
+        title = "Error !"
+        description = response["message"]
+        colour = discord.Colour.brand_red()
+        img_url = assets.restaurants.FAIL
+    else:
+        title = "Restaurants refilled!"
+        description = f"You spent {response['total_cost']}両 and ordered a total of {response['total_added_ramen']} bowl(s) for your restaurant(s)."
+        colour = discord.Colour.brand_green()
+        img_url = assets.restaurants.RAMEN
+    
+    return title, description, colour, img_url

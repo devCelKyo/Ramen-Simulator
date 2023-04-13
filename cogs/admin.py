d058d0f2fd68e7aa257d565090010e38a12f9f25
devCelKyo <sangoku80090@gmail.com>
2023-04-13 17:22:16 +0200

    1.5.1

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,6 +8,26 @@ class Admin(commands.Cog):
     
     @commands.command()
     @utils.permissions.admin()
-    async def broadcast(self, ctx, message):
+    async def broadcast(self, ctx, message, everyone):
+        text = ""
+        if everyone == "1":
+            text += "@everyone : "
+        text += f"System announcement\n {message}"
+
         for guild in self.bot.guilds:
-            await guild.system_channel.send(content=f"@everyone : System announcement\n {message}")
+            await guild.system_channel.send(content=text)
+    
+    @commands.command()
+    @utils.permissions.admin()
+    async def file_broadcast(self, ctx, everyone):
+        text = ""
+        if everyone == "1":
+            text += "@everyone : "
+        file = open("broadcast.txt", "r")
+        message = file.read()
+        file.close()
+        
+        text += f"System announcement\n {message}"
+
+        for guild in self.bot.guilds:
+            await guild.system_channel.send(content=text)

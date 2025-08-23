7ef755fe2e041747f17c1380b8ed2c04434e4ea1
devCelKyo <sangoku80090@gmail.com>
2025-08-23 16:14:18 +0200

    nuke for revamping

--- assets/restaurants.py
+++
@@ -1,6 +0,0 @@
-RAMEN = "https://cdn.discordapp.com/attachments/778283670720479236/1080541950874239016/image.png"
-RYO = "https://cdn.discordapp.com/attachments/778283670720479236/1081368999838879774/latest.png"
-NO_MONEY = "https://cdn.discordapp.com/attachments/778283670720479236/1081372306795921539/Dg0n6jhVQAEUSIz.png"
-FAIL = "https://cdn.discordapp.com/attachments/778283670720479236/1081372763492728972/latest.png"
-SHOP = "https://cdn.discordapp.com/attachments/778283670720479236/1081374439695978516/latest.png"
-HOKAGE = "https://cdn.discordapp.com/attachments/778283670720479236/1084562146949599232/latest.png"
--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import discord
+from discord.ext.commands import Bot
+
 import os
-
-from asyncio import run
-
-# Cogs classes import
-from cogs.bases import Bases
-from cogs.shops import Shops
-from cogs.admin import Admin
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-Bot = discord.ext.commands.Bot(command_prefix='r', intents=intents, allowed_mentions=discord.AllowedMentions(everyone = True))
-async def add_cogs():
-    await Bot.add_cog(Bases(Bot))
-    await Bot.add_cog(Shops(Bot))
-    await Bot.add_cog(Admin(Bot))
-    # add all others cogs
-
-run(add_cogs())
-Bot.run(os.environ['hashirama_bot_token'])
+bot = Bot(command_prefix='r', intents=intents, allowed_mentions=discord.AllowedMentions(everyone = True))
+bot.run(os.getenv('hashirama_bot_token'))

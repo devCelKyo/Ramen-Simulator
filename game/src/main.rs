9d5f4f0eb01d4bbbdae13fc0ee0e4d06e2ed3271
devCelKyo <sangoku80090@gmail.com>
2025-09-20 01:30:16 +0200

    reimplementing restaurant and ingredient basics

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Welcome to Ramen Simulator!");
+}
+
+struct Restaurant {
+    name: String,
+    cash: f64,
+}
+
+enum IngredientType {
+    Broth,
+    Noodles,
+    Protein,
+    Vegetable,
+}
+
+struct Ingredient {
+    name: String,
+    ing_type: IngredientType,
+}
+
+struct Inventory {
+    stocks: HashMap<Ingredient, i32>,
+}
+
+struct Receipe {
+    broth: Ingredient,
+    noodles: Ingredient,
+    proteins: Vec<Ingredient>,
+    vegetables: Vec<Ingredient>,
 }

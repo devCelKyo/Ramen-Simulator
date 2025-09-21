50013d94f1f855198579f1df3da6e8f7478100cb
devCelKyo <sangoku80090@gmail.com>
2025-09-21 16:26:28 +0200

    orderqueue

--- game/src/restaurants/order.rs
+++ game/src/restaurants/order.rs
@@ -1,5 +1,35 @@
+use std::collections::VecDeque;
 use super::Ramen;
 
 pub struct Order {
-    ramen: Ramen,
+    pub ramen: Ramen,
+}
+
+pub struct OrderQueue {
+    orders: VecDeque<Order>
+}
+
+impl OrderQueue {
+    pub fn new() -> Self {
+        Self {
+            orders: VecDeque::new()
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.orders.is_empty()
+    }
+
+    pub fn size(&self) -> usize {
+        self.orders.len()
+    }
+
+    pub fn pop_first(&mut self) -> Order {
+        assert!(!self.is_empty());
+        self.orders.pop_front().unwrap()
+    }
+
+    pub fn place_order(&mut self, order: Order) {
+        self.orders.push_back(order);
+    }
 }

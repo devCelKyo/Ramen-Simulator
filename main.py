586b4b2bf3ceac479b03cb035c6504866d98240c
devCelKyo <sangoku80090@gmail.com>
2025-10-05 22:51:13 +0200

    dev config

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = Bot(command_prefix='r', intents=intents, allowed_mentions=discord.AllowedMentions(everyone = True))
-bot.run(os.getenv('hashirama_bot_token'))
+
+#bot.run(os.getenv('hashirama_bot_token'))
+bot.run(os.getenv('tsunade_bot_token'))

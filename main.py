b990a5b4269838c9d625d6ccc4cdb05b7843aab3
devCelKyo <sangoku80090@gmail.com>
2025-10-06 22:38:45 +0200

    playing around with bot

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ intents.message_content = True
 
 bot = Bot(command_prefix='r', intents=intents, allowed_mentions=discord.AllowedMentions(everyone = True))
 
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("pong")
+
+@bot.command()
+async def home(ctx):
+    await ctx.channel.send("home")
+
 #bot.run(os.getenv('hashirama_bot_token'))
 bot.run(os.getenv('tsunade_bot_token'))
